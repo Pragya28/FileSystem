4e7dc689b10388a859b9a1f9121c097827319be3
Pragya <pragyajha2812@msitprogram.net>
2021-10-01 17:48:49 +0530

    trying deployment

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/createAccount', methods=['POST'])
+def create():
+    if request.method == 'POST':
+        return render_template('createAccount.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        return render_template('success.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
