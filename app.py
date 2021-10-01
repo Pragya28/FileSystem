1bc81dd9bfe6350e049cd2fa1f2e9a53296fc967
Pragya <pragyajha2812@msitprogram.net>
2021-10-02 02:27:58 +0530

    updated app.py

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ def login():
         print(username, password)
         return render_template('success.html')
 
+@app.route('/create', methods=['POST'])
+def created():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        return render_template('success.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
