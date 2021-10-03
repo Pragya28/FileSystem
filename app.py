02b9ddde4d219f380408ab7e23184fe143f17487
Pragya <pragyajha2812@msitprogram.net>
2021-10-03 15:33:40 +0530

    updated db

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from account import createAccount
 
 app = Flask(__name__)
 
-ENV = "prob"
+ENV = "prod"
 
 if ENV == "dev":
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jha@localhost/filesystem-data'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://faczclvhitvdwh:ae3089dce8da2eadfc810e426aa0d464be78d7fdb44a8d3792c94fbd5bfadd0a@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5rg047ldjvf8m'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cdawzcdbskcdpd:750487e3896466ef4bf5a34f02f730301d1e861377118ee0cd55e86935200a9b@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d7rf7uhpqn1ite'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
