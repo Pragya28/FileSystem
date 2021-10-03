166321e87a1bb9c92f4d3d5510d53845c719d800
Pragya <pragyajha2812@msitprogram.net>
2021-10-03 14:07:59 +0530

    updated db

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if ENV == "dev":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jha@localhost/filesystem-data'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://faczclvhitvdwh:ae3089dce8da2eadfc810e426aa0d464be78d7fdb44a8d3792c94fbd5bfadd0a@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5rg047ldjvf8m'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://faczclvhitvdwh:ae3089dce8da2eadfc810e426aa0d464be78d7fdb44a8d3792c94fbd5bfadd0a@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5rg047ldjvf8m'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
