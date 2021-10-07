8382f7490a0ce75d9a0b74099de97cf147834d9e
Pragya <pragyajha2812@msitprogram.net>
2021-10-07 10:21:14 +0530

    updated database

--- app.py
+++ app.py
@@ -23,12 +23,11 @@ db.init_app(app)
 login.init_app(app)
 login.login_view = 'login'
  
-'''
 @app.before_first_request
 def create_all():
     # db.drop_all()
     db.create_all()
-'''
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if current_user.is_authenticated:
