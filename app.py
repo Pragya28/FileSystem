429f3265e6e9aa34942bfd0547875500a827b50f
Pragya <pragyajha2812@msitprogram.net>
2021-10-07 22:05:55 +0530

    updated

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if ENV == "dev":
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
